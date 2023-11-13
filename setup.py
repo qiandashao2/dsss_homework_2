067f16820f17f42a86f96ab60e95048d34845d64
qiandashao2 <2320358989@qq.com>
2023-11-13 02:45:19 +0100

    final decision

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='math_quiz',
+    version="1.0",
+    author="Zekai Qian",
+    author_email="2320358989@qq.com",
+    packages=find_packages(),
+    install_requires=["random", "unittest"]
+
+)
