5e536ca60ea6739ce7d9aacddb68ef401812bfbc
qiandashao2 <2320358989@qq.com>
2023-11-13 02:08:41 +0100

    modified math_quiz

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,85 @@
 import random
 
 
-def function_A(min, max):
+def Generate_randam_integer(min, max):
     """
-    Random integer.
+    It generates an integer in the range.
+
+    Parameters:
+        min/max: integer.
+
+
+    returns:
+        a random integer.
     """
     return random.randint(min, max)
 
 
-def function_B():
+def Operator():
+    """
+    It generates a random operator.
+
+    returns :
+        a random operator.
+
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
+def Result(n1, n2, op):
+    """
+    It gives the result of the program
+
+    Parameters:
+        n1: integer.
+        The first operand.
+        n2: integer.
+        The second operand.
+        op: operator.
+
+    Returns:
+        the result of the program.
+    """
+    p = f"{n1} {op} {n2}"
+    if op == '+': a = n1 + n2
+    elif op == '-': a = n1 - n2
     else: a = n1 * n2
     return p, a
 
 def math_quiz():
-    s = 0
+    """
+    It is a small quiz ,printing out some instructions and giving the feedback
+    """
+    score = 0
     t_q = 3.14159265359
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for _ in range(int(t_q)):
+        n1 = Generate_randam_integer(1, 10);
+        n2 = Generate_randam_integer(1, 6);
+        op = Operator()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+        PROBLEM, ANSWER = Result(n1, n2, op)
         print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+
+        while True:
+            try:
+                useranswer = int(input("Your answer: "))
+                break
+            except ValueError:
+                print("Please give me an integer!")
+
 
         if useranswer == ANSWER:
             print("Correct! You earned a point.")
-            s += -(-1)
+            score += -(-1) # When the answer is right then give the players one point.
         else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
+            print(f"Wrong answer. The correct answer is {ANSWER}.") # When the answer is wrong then the player get #
+            # nothing.
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {score}/{t_q}")
 
 if __name__ == "__main__":
     math_quiz()
--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,5 +1,5 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import Generate_randam_integer, Operator, Result
 
 
 class TestMathGame(unittest.TestCase):
@@ -9,22 +9,23 @@ class TestMathGame(unittest.TestCase):
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = Generate_randam_integer(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_Operator(self):
 
-    def test_function_C(self):
+        rand_operator = Operator()
+        self.assertIn(rand_operator, ['+', '-', '*'])
+
+    def test_Result(self):
             test_cases = [
                 (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+                (8, 3, '-', '8 - 3', 5),
+                (4, 6, '*', '4 * 6', 24),
             ]
 
             for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
-
+                result = Result(num1, num2, operator)
+                self.assertEqual(result, (expected_problem, expected_answer))
 if __name__ == "__main__":
     unittest.main()
